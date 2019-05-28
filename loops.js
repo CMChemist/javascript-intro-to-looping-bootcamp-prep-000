bf24d810b26701cd135985aebe15d25c42c3b477
global-value-0259 <josephtrettevik@gmail.com>
2019-05-28 16:54:43 +0000

    Automatically backed up by Learn

---
+++ loops.js
@@ -0,0 +1,31 @@
+function forLoop(localArray) {
+  for (let i = 0; i < 25; i++) {
+     if (i === 1) {
+        localArray.push('I am 1 strange loop.')
+      } else {
+          localArray.push("I am "+`${i}`+" strange loops.")
+      }
+  }
+  return localArray
+}
+/*
+function whileLoop (number) {
+  while (number > 0){
+    console.log(--number)
+  }
+  return 'done'
+}
+
+
+function incrementVariable() {
+  i = i + 1;
+  return i;
+}
+
+function doWhileLoop (integer) {
+  i = 0;
+  do {
+    console.log('I run once regardless.')
+  } while (incrementVariable() < integer)
+  }
+}
